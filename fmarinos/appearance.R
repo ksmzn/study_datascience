a990f634d3618099c03f20fd7169db67e7fbfb58
ksmzn <kossyfm@outlook.com>
2017-01-19 21:56:43 +0900

    Fix to search all sections

--- fmarinos/appearance.R
+++ fmarinos/appearance.R
@@ -6,6 +6,21 @@ library(stringr)
 library(rvest)
 library(dplyr)
 
+base_url <- 'http://www.football-lab.jp'
+
+# 全節へのリンクを取得
+first_url <- 'http://www.football-lab.jp/y-fm/report/'
+base_html <- read_html(first_url)
+sections <- base_html %>% html_nodes("div.linkPast a") %>%
+  html_attr("href") %>% 
+  str_c(base_url, .)
+  # html_structure()
+
+# 各節の対戦カードへのリンクを取得
+matches <- base_html %>% html_nodes("div.cardtab a") %>%
+  html_attr("href") %>% 
+  str_c(base_url, .)
+
 url <- 'http://www.football-lab.jp/y-fm/'
 team_html <- read_html(url)
 team_html_sjis <- team_html %>% iconv(from = "UTF-8")
@@ -13,8 +28,8 @@ team_html %>% html_table()
 team_players <- team_html %>% 
   html_node('#sorTable') %>% 
   html_table()
-colnames(team_players) <- c("Position", "Number", "Name", "Games", "Starting", "Apps", "Attack", "Pass", "Dribble", "Cross", "Shoot", "Defense", "Save")
 team_players <- team_players %>% 
+  magrittr::set_colnames(c("Position", "Number", "PlayerName", "Games", "Starting", "Apps", "Attack", "Pass", "Dribble", "Cross", "Shoot", "Defense", "Save")) %>% 
   as_data_frame()
 
 transfer_url <- 'http://www.jleague.jp/special/transfer/2017winter/'
@@ -25,9 +40,21 @@ transfer_team <- transfer_html %>%
   html_node('.nyc2colRight') %>% 
   html_node('.dataTable') %>% 
   html_table()
-colnames(transfer_team) <- C
-transfer_team %>% 
+
+transfer_name <- transfer_team %>% 
   as_data_frame %>% 
-  rename(Name = X1)1
-j１jjjjjjjjjj
-  mutate()
+  rename(PlayerName = X1) %>% 
+  slice(2:n()) %>% 
+  mutate(PlayerName = str_replace(PlayerName, '［.*', '')) %>% 
+  mutate(transfer_flg = 1)
+transfer_name
+
+team_players %>% 
+  left_join(transfer_name, by = "PlayerName") %>% 
+  mutate(AppsRate=Apps / sum(Apps)) %>% 
+  arrange(desc(AppsRate)) %>% View
+  group_by(transfer_flg) %>% 
+  summarise(Time=sum(Apps)) %>% 
+  ungroup()
+  
+  View
